65b9120acaf1c77da93281ed4c63d6b6fd63c158
master.cins.china <master.cins.china@gmail.com>
2016-03-28 16:51:29 +0800

    chg git url

--- buildout.rb
+++ buildout.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Buildout < Formula
     homepage 'https://github.com/cins-china/cins-buildout'
-    url 'git@github.com:cins-china/cins-buildout.git', :tag => '2.5.1'
+    url 'https://github.com/cins-china/cins-buildout.git', :tag => '2.5.1'
     version '2.5.1'
 
     def install
